00013a3a4d333e5a355c5276a9c5fa213829c8bc
simonstreuli <112710583+simonstreuli@users.noreply.github.com>
2023-01-03 16:13:45 +0100

    funktionierendes Grundgerüst

---
+++ main.js
@@ -0,0 +1,34 @@
+async function getNews() {
+  await fetch(
+    "https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=E44ehj1sHkc6t7dVll7i60DOab5vBF05"
+  )
+    .then((d) => d.json())
+    .then((response) => {
+      console.log(response.results);
+      for (var i = 0; i < response.results.length; i++) {
+        const output = document.getElementById("output");
+
+        try {
+          output.innerHTML += `
+                    <div class="card">
+                    <div class="card-body">
+                    <img src="${response.results[i]["media"][0]["media-metadata"][2].url}" class="card-img-top" alt="${response.results[i]["media"][0].caption}" title="${response.results[i]["media"][0].caption}"><br>
+                    <h2 class="card-title">${response.results[i].title}</h2>
+                    <div class="card-text">
+                        <p>${response.results[i].abstract}</p>
+                    </div>
+                    </div>
+                    </div>
+                    <br>
+                    `;
+          console.log(response.results[i]["media"][0].caption);
+        } catch (err) {
+          console.log(err);
+        }
+        // console.log(response.results[i].title);
+        // console.log(i);
+      }
+      document.getElementById("copyright").innerHTML = response.copyright;
+    });
+}
+getNews();
